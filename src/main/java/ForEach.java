63fa9902b2940a0181f3a517a7992f71bb25e23e
Seowoo <5w31892p@gmail.com>
2022-12-24 20:04:33 +0900

    Add: foreach문

---
+++ src/main/java/ForEach.java
@@ -0,0 +1,21 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class ForEach {
+    public static void main(String[] args) {
+
+        // foreach문은 직관적
+        // 배열과 리스트만 가능함
+        String[] num = {"하나", "둘", "셋"};
+        for (String number : num) {
+            System.out.println(number);
+        }
+        System.out.println("-----------");
+
+        ArrayList<String> n = new ArrayList<>(Arrays.asList("하나", "둘", "셋"));
+        System.out.println(n);
+        for (String numbers : n) {
+            System.out.println(numbers);
+        }
+    }
+}
